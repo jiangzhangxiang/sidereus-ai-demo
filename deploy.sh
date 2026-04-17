da3b011481f6276bbd2ffc7b23c5d8e64b3916b7
jiangzhangxiang <yyy67239667@163.com>
2026-04-17 18:49:58 +0800

    fix: 修复部署脚本中的错误问题

--- deploy.sh
+++ deploy.sh
@@ -33,6 +33,10 @@ install_dependencies() {
         ufw > /dev/null 2>&1
 
     if ! command -v docker &> /dev/null; then
+        install -m 0755 -d /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+        chmod a+r /etc/apt/keyrings/docker.asc
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
         apt-get update -qq > /dev/null 2>&1
         apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null 2>&1
     fi
